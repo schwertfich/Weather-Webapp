74c7adb58fd5d1bb46bf1d35d2847cef437ea6fc
Leif <leifjahnel@gmail.com>
2021-05-27 12:37:19 +0200

    added line chart variables

--- static/js/main.js
+++ static/js/main.js
@@ -1,3 +1,4 @@
+// Function for location
 var x = document.getElementById("cords");
     
     function getLocation() {
@@ -30,3 +31,51 @@ var x = document.getElementById("cords");
           break;
       }
     }
+
+// Line Chart for Forecast
+
+var xmlhttp = new XMLHttpRequest();
+var url = "https://192.168.178.25/static/js/hourlyforecast.json"
+xmlhttp.open("GET", url, true);
+xmlhttp.send();
+xmlhttp.onreadystatechange = function(){
+  if(this.readyState == 4 && this.status == 200){
+    var data = JSON.parse(this.responseText);
+    //console.log(data);
+    var time = data.hourly.map(function(elem) {
+      return elem.dt;
+    });
+    //console.log(time)    
+    var hour = data.hourly.map(function(elem) {
+      return elem.temp;
+    });
+    //console.log(hour);
+
+    var ctx = document.getElementById('canvas').getContext('2d');
+    var myChart = new Chart(ctx, {
+        type: 'line',
+        data: {
+            labels: time,
+            datasets: [{
+                label: 'Tempereture',
+                data: hour,
+                backgroundColor: 'transperent',
+                borderColor: "red",
+                borderWidth: 4
+            }]
+        },
+        options: {
+          elements:{
+            line:{
+              tension:0.1
+            }
+          },
+            scales: {
+                y: {
+                    beginAtZero: true
+                }
+            }
+        }
+    });
+  }
+}
